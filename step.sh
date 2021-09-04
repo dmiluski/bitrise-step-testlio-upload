42340cc08ec66cebcc1a65599360c9584e40cb5a
Dane Miluski <dmiluski@gmail.com>
2021-09-03 20:19:34 -0700

    Initial Commit
    Initial Bitrise Implementation for uploading IPA files to Testlio
    
    Changes:
    + yml, shell

---
+++ step.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+curl --location --request POST "https://api.testlio.com/build/v3/collections/${testlio_collection_id}/builds" \
+-F "file=@$BITRISE_IPA_PATH" \
+--header 'Content-Type: multipart/form-data' \
+--header "Authorization: Bearer ${TESTLIO_API_TOKEN}"
