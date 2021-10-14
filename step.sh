428ca8b0f6f0001181110fbcbd1b4e9f142ebd71
Dane Miluski <dmiluski@gmail.com>
2021-10-14 14:49:06 -0700

    Include AppName Param
    Goal:
    Include App Name Param for submission
    
    Changes:
    + Update yml
    + Update curl args

--- step.sh
+++ step.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-set -ex
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 
 curl --location --request POST "https://api.testlio.com/build/v3/collections/${testlio_collection_id}/builds" \
 -F "file=@$BITRISE_IPA_PATH" \
+-F name="${TESTLIO_APP_NAME}" \
 --header 'Content-Type: multipart/form-data' \
 --header "Authorization: Bearer ${TESTLIO_API_TOKEN}"
